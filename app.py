16d860fb666b7df3234f527b66ea3af10c593a9e
samaysingh2612 <samaythakur26@gmail.com>
2026-07-18 22:42:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import streamlit as st
+import pickle
+from posters import fetch_poster
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+
+    movie_list = sorted(
+        list(enumerate(distances)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
+
+    recommended_movies = []
+    recommended_posters = []
+
+    for i in movie_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_posters.append(fetch_poster(movie_id))
+
+    return recommended_movies, recommended_posters
+
+movies = pickle.load(open("movie_list.pkl", "rb"))
+similarity = pickle.load(open("similarity.pkl", "rb"))
+
+st.title("🎬 Movie Recommendation System")
+
+selected_movie = st.selectbox(
+    "Choose a Movie",
+    movies['title'].values
+)
+
+if st.button("Recommend"):
+
+    names, posters = recommend(selected_movie)
+
+    col1,col2,col3,col4,col5 = st.columns(5)
+
+    with col1:
+        st.image(posters[0])
+        st.write(names[0])
+
+    with col2:
+        st.image(posters[1])
+        st.write(names[1])
+
+    with col3:
+        st.image(posters[2])
+        st.write(names[2])
+
+    with col4:
+        st.image(posters[3])
+        st.write(names[3])
+
+    with col5:
+        st.image(posters[4])
+        st.write(names[4])
