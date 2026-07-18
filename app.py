8da582f66159b4cc1f393b1f8f1987d4baa86581
samaysingh2612 <samaythakur26@gmail.com>
2026-07-18 22:49:25 +0530

    Enhance recommendation system with CountVectorizer
    Added CountVectorizer to process movie tags for improved recommendations.

--- app.py
+++ app.py
@@ -24,7 +24,12 @@ def recommend(movie):
     return recommended_movies, recommended_posters
 
 movies = pickle.load(open("movie_list.pkl", "rb"))
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
+cv = CountVectorizer(max_features=5000, stop_words="english")
+
+vectors = cv.fit_transform(movies["tags"]).toarray()
 
 similarity = cosine_similarity(vectors)
 
